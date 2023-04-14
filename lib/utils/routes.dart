df91595b27e74747abdf765a4a7a339e807d9a6b
Rohit Raj <2006280@kiit.ac.in>
2023-04-15 00:39:36 +0530

    pta nhi

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -5,5 +5,6 @@ class MyRoutes {
   static String signupscreen = "/signup";
   static String profilepage = "/profile";
   static String infopage = "/info";
+  static String create_event = "/create";
   // static String mainpage = "/main";
 }
