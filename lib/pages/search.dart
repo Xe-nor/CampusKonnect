09a1d664744d40f9762a3c3d76100a241e5194ef
Rohit Raj <2006280@kiit.ac.in>
2023-04-17 19:15:31 +0530

    Build Search page

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/textform.dart';
+
 // ignore: camel_case_types
 class search extends StatelessWidget {
   const search({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.search),
+        elevation: 2,
+        backgroundColor: Colors.black,
+        title: Container(
+          width: double.infinity,
+          height: 35,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(3)),
+          child: textform(
+            prefixIcon: Icons.search,
+            hinttxt: "Enter the event you looking for",
+            labeltxt: null,
+            isObscure: false,
+          ),
+        ),
+      ),
+      body: const SingleChildScrollView(),
+    );
   }
 }
