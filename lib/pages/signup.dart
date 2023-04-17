017c6707005a8f7fa9759ea9bffaae8443ae57dc
mayurrajsarma <mayurrajsarma202@gmail.com>
2023-04-17 19:10:41 +0530

    Added firebase

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -2,7 +2,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 //import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:campuskonnect/pages/homepage.dart';
 import 'package:campuskonnect/pages/loginpage.dart';
 import 'package:campuskonnect/pages/profilepage.dart';
 import 'package:campuskonnect/pages/bottomnavbar.dart';
