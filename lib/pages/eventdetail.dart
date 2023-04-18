b207d4cfafa8832f570c0bd0e2f2e8efe23d7df5
Dhriti069 <98838127+Dhriti069@users.noreply.github.com>
2023-04-18 20:20:25 +0530

    eventdetail

--- lib/pages/eventdetail.dart
+++ lib/pages/eventdetail.dart
@@ -1,8 +1,12 @@
+import 'package:campuskonnect/utils/theme.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:campuskonnect/pages/bottomnavbar.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../widgets/icondetailpair.dart';
 
 // ignore: camel_case_types
 class eventdetail extends StatefulWidget {
@@ -49,118 +53,190 @@ class _eventdetailState extends State<eventdetail> {
                 ),
               ),
               Positioned(
-                  top: 200,
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height,
-                    decoration: const BoxDecoration(
-                        color: Color.fromARGB(255, 29, 29, 51),
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(30),
-                            topRight: Radius.circular(30))),
-                    child: Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Column(
-                        children: [
-                          Row(
+                top: 200,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 29, 29, 51),
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30))),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text("EVENT-NAME",
+                                style: GoogleFonts.urbanist(
+                                    fontSize: 26.0,
+                                    fontWeight: FontWeight.w700))
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        icondetailpair(
+                            detail: 'Location',
+                            icon: FontAwesomeIcons.locationPin),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        icondetailpair(
+                            detail: 'Date', icon: FontAwesomeIcons.calendar),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        const SizedBox(
+                          height: 60,
+                        ),
+                        Row(
+                          children: [
+                            Text("MORE INFORMATION",
+                                style: GoogleFonts.urbanist(
+                                    fontSize: 26.0,
+                                    fontWeight: FontWeight.w700))
+                          ],
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        icondetailpair(
+                            detail: 'Total-Time', icon: FontAwesomeIcons.clock),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        icondetailpair(
+                            detail: 'Branch',
+                            icon: FontAwesomeIcons.bookOpenReader),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        icondetailpair(
+                            detail: 'Batch', icon: FontAwesomeIcons.user),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const SizedBox(
+                          height: 60,
+                        ),
+                        Row(
+                          children: [
+                            Text("DESCRIPTION",
+                                style: GoogleFonts.urbanist(
+                                    fontSize: 26.0,
+                                    fontWeight: FontWeight.w700))
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 230,
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          child: Row(
                             children: [
-                              Text("EVENT-NAME",
-                                  style: GoogleFonts.urbanist(
-                                      fontSize: 26.0,
-                                      fontWeight: FontWeight.w700))
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          Row(
-                            children: const [
-                              Icon(Icons.date_range, color: Colors.grey),
-                              Padding(
-                                padding: EdgeInsets.symmetric(horizontal: 5),
-                                child: Text(
-                                  "date and time",
-                                  style: TextStyle(color: Colors.grey),
-                                ),
+                              AppButtons(
+                                  size: 60,
+                                  color: Color.fromARGB(255, 255, 255, 255),
+                                  isIcon: true,
+                                  icon: FontAwesomeIcons.heart,
+                                  backgroundColor:
+                                      Color.fromARGB(87, 87, 76, 244),
+                                  borderColor: Appcolors.buttoncolor),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              ResponsiveButton(
+                                isResponsive: true,
+                                width: double.maxFinite,
+                              ),
+                              const SizedBox(
+                                width: 20,
                               ),
                             ],
                           ),
-                          Row(
-                            children: [
-                              const Icon(Icons.location_on,
-                                  color: Colors.white),
-                              Text(
-                                "location",
-                                style: Theme.of(context).textTheme.labelLarge,
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 60,
-                          ),
-                          Row(
-                            children: const [
-                              Text(
-                                "MORE INFORMATION",
-                                style: TextStyle(fontSize: 30),
-                              )
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Row(
-                            children: const [
-                              Icon(Icons.bookmark_add, color: Colors.white),
-                              Text(
-                                "TOTAL-TIME",
-                                style: TextStyle(fontSize: 20),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: const [
-                              Icon(Icons.bookmark_add, color: Colors.white),
-                              Text(
-                                "BATCH",
-                                style: TextStyle(fontSize: 20),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 60,
-                          ),
-                          Row(
-                            children: const [
-                              Text(
-                                "DESCRIPTION",
-                                style: TextStyle(fontSize: 30),
-                              )
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 100,
-                          ),
-                          Positioned(
-                            bottom: 0,
-                            left: 10,
-                            child: Row(
-                              children: [
-                                IconButton(
-                                  style: const ButtonStyle(),
-                                  onPressed: () {},
-                                  icon: const Icon(
-                                      Icons.favorite_border_outlined),
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  ))
+                  ),
+                ),
+              )
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class AppButtons extends StatelessWidget {
+  final Color color;
+  String? text;
+  IconData? icon;
+  final Color backgroundColor;
+  double size;
+  final Color borderColor;
+  bool? isIcon;
+
+  AppButtons(
+      {Key? key,
+      this.isIcon = false,
+      this.text = "Hi",
+      this.icon,
+      required this.size,
+      required this.color,
+      required this.backgroundColor,
+      required this.borderColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+          border: Border.all(
+            color: borderColor,
+            width: 1.0,
+          ),
+          borderRadius: BorderRadius.circular(15),
+          color: backgroundColor),
+      child: isIcon == false
+          ? Center(child: Text("Register"))
+          : Center(child: Icon(icon, color: color)),
+    );
+  }
+}
+
+class ResponsiveButton extends StatelessWidget {
+  bool? isResponsive;
+  double? width;
+  ResponsiveButton({Key? key, this.width = 120, this.isResponsive = false})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Container(
+        width: isResponsive == true ? double.maxFinite : width,
+        height: 60,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10), color: Colors.white),
+        child: Row(
+          mainAxisAlignment: isResponsive == true
+              ? MainAxisAlignment.spaceBetween
+              : MainAxisAlignment.center,
+          children: [
+            isResponsive == true
+                ? Container(
+                    margin: const EdgeInsets.only(left: 20),
+                    child: const Text("Register"),
+                    color: Colors.blue,
+                  )
+                : Container(),
+            //sImage.asset("img/button-one.png"),/
+          ],
         ),
       ),
     );
